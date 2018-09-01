9ffa5d3d402a18833a1f76393b99e4c15c018a2c
sennedjem <sebastianariell@hotmail.com.ar>
2018-09-01 16:50:42 -0300

    api devuelve headers cors

--- config/app.php
+++ config/app.php
@@ -167,7 +167,6 @@ return [
         /*
          * Package Service Providers...
          */
-        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -178,6 +177,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Laravel\Socialite\SocialiteServiceProvider::class,
+        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
+        Barryvdh\Cors\ServiceProvider::class,
 
     ],
 
