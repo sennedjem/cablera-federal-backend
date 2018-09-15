b1a0dbe3b0194192bdaf54d346bbdf8e9dbe7d4f
sennedjem <sebastianariell@hotmail.com.ar>
2018-09-15 18:32:59 -0300

    agregado comando update posts a kernel laravel

--- app/Console/Commands/getPosts.php
+++ app/Console/Commands/getPosts.php
@@ -38,14 +38,14 @@ class getPosts extends Command{
      * @return mixed
      */
     public function handle(){
+        \Log::info('handle!');
         foreach (Site::where('type','Twitter')->cursor() as $site) {
             $this->updateTwitterSitePosts(($site->url));
         }
     }
 
     public function updateTwitterSitePosts($account){
-        $client = new Client();
-        $response = $client -> get('http://twitrss.me/twitter_user_to_rss/?user='.$account);
+        $response = $this->getPosts($account);
         $xml = $response->xml();
         $items = $xml->channel->item;
         for ($i=0; $i<count($items); $i++) {
@@ -53,6 +53,11 @@ class getPosts extends Command{
                 $this->addTwitterPost($items[$i],$account);
             }
         }
+    }
+
+    public function getPosts($account){
+        $client = new Client();
+        return $client -> get('http://twitrss.me/twitter_user_to_rss/?user='.$account);
     }
 
     public function addTwitterPost($item,$account){
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,9 +12,10 @@ class Kernel extends ConsoleKernel
      *
      * @var array
      */
-    protected $commands = [
-        //
+     protected $commands = [
+        'App\Console\Commands\getPosts'
     ];
+
 
     /**
      * Define the application's command schedule.
@@ -22,21 +23,19 @@ class Kernel extends ConsoleKernel
      * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
      * @return void
      */
-    protected function schedule(Schedule $schedule)
-    {
-        // $schedule->command('inspire')
-        //          ->hourly();
+    protected function schedule(Schedule $schedule){
+        $schedule->command('posts:update')->cron('*/'.env('UPDATE_TIME', '1').' * * * *');
     }
 
     /**
      * Register the commands for the application.
      *
      * @return void
-     */
     protected function commands()
     {
         $this->load(__DIR__.'/Commands');
 
         require base_path('routes/console.php');
     }
+     */
 }
