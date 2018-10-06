c37dd02a8c046c476f546efd32110fdabcc3aa19
sennedjem <sebastianariell@hotmail.com.ar>
2018-10-06 19:27:32 -0300

    se agregan transacciones a posts controller

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests\Post\Update;
 use App\Models\Post;
 use App\Models\Tag;
+use DB;
 
 class PostsController extends Controller{
     //
@@ -29,13 +30,21 @@ class PostsController extends Controller{
      */
     public function update(Update $request, $id){
         //
-        $post = Post::find($id);
-        $tags = $request->input('tags');
-        $post -> fill($request->validated());
-        $post -> save();
-        Tag::ifDoesntExistCreate($tags);
-        $post->tags()->sync(array_map('strtolower', $tags));
+    	DB::beginTransaction();
+	    try {
+	        $post = Post::find($id);
+	        $tags = $request->input('tags');
+	        $post -> fill($request->validated());
+	        $post -> save();
+	        Tag::ifDoesntExistCreate($tags);
+	        $post->tags()->sync(array_map('strtolower', $tags));
 
-    	return response() -> json($post);
+			DB::commit();
+    		return response() -> json($post);
+	    } catch (\Exception $e) {
+			$error = $e->getMessage();
+			DB::rollback();
+			return response() -> json($error,500);
+	    }
     }
 }
