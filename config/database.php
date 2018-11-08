06d4572146d992671627f2c3e71bff36894b8ee6
Dario Gutierrez <dariofg93@gmail.com>
2018-11-07 23:41:40 -0300

    deploy heroku

--- config/database.php
+++ config/database.php
@@ -1,13 +1,13 @@
 <?php
 
-/*
+
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
 $host = $url["host"];
 $username = $url["user"];
 $password = $url["pass"];
 $database = substr($url["path"], 1);
-*/
+
 
 return [
 
@@ -51,10 +51,10 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'port' => env('DB_PORT', '3306'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => env($host, '127.0.0.1'),
+            'database' => env($database, 'forge'),
+            'username' => env($username, 'forge'),
+            'password' => env($password, ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
