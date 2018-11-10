02593139ffc29c81d14ba79ade283e928078a54d
Dario Gutierrez <dariofg93@gmail.com>
2018-11-10 01:29:45 -0300

    tamaño maximo de datos guardados cambiado a 191

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,17 +2,20 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Validator;
 use App\Models\Site;
 
-class AppServiceProvider extends ServiceProvider{
+class   AppServiceProvider extends ServiceProvider{
     /**
      * Bootstrap any application services.
      *
      * @return void
      */
     public function boot(){
+        Schema::defaultStringLength(191);
+
         Validator::extend('uniqueTypeAndUrl', function ($attribute, $value, $parameters, $validator) {
             $sites = Site::where('url', $value)
                                 ->where('type', $parameters[0]);
